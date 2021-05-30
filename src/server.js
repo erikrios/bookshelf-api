a737ee6f512fc17c9af6b3bc7ced21f5190e9a3b
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-05-30 22:13:28 +0700

    Create server.js

---
+++ src/server.js
@@ -0,0 +1,21 @@
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: process.env.PORT || 5000,
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : 'io.erikrios.github',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  server.route(routes);
+
+  await server.start();
+  console.log(`Server running on ${server.info.uri}`);
+};
+
+init();
